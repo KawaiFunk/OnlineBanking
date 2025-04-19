d1210af190672c5e1adf55183e760a6ebecfe57d
Damian Dan <dan.damian@isa.utm.md>
2025-04-20 02:21:27 +0300

    Implemented login and session. Also admin attributes.

---
+++ Web/Attributes/RequireLoginAttribute.cs
@@ -0,0 +1,41 @@
+﻿using AizenBankV1.Web.Extensions;
+using Core.Services.UserServices;
+using Domain.Data.Context;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Web.Attributes
+{
+    public class RequireLoginAttribute : ActionFilterAttribute
+    {
+        private readonly IUserService _userService;
+
+        public RequireLoginAttribute()
+        {
+            BankDbContext context = new BankDbContext();
+            _userService = new UserService(context);
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var apiCookie = HttpContext.Current.Request.Cookies["X-KEY"];
+            if (apiCookie != null)
+            {
+                var profile = _userService.GetUserByCookie(apiCookie.Value);
+                if (profile == null)
+                {
+                    filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "User", action = "Login" }));
+                }
+                else
+                {
+                    HttpContext.Current.SetMySessionObject(profile);
+                }
+            }
+            else
+            {
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary(new { controller = "User", action = "Login" }));
+            }
+        }
+    }
+}
